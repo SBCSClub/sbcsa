3d891a4b7b34a0ed834161e390664b6ec5aa0f29
mahitmehta <mahit.py@gmail.com>
2024-01-25 10:39:45 -0500

    WIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,126 +1,162 @@
 "use client";
 
-import Image from 'next/image'
-import Animate from '@/components/Animate';
-import styled from 'styled-components';
+import { Canvas } from "@react-three/fiber"
+import * as THREE from 'three';
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import { useEffect, useMemo, useRef } from 'react';
+import { animated } from "@react-spring/three";
+import Image from "next/image";
+import styled from "styled-components";
 
-// import { useEffect } from 'react';
-// import Scrollbar from 'smooth-scrollbar';
+const Lights = () => {
+  return (
+    <>
+      <ambientLight intensity={4} castShadow />
+      <pointLight intensity={3}/>
+    </>
+  );
+};
 
-const ShimmeringH1 = styled.h1`
-background-image: linear-gradient(90deg, #8C8796 0%, white 40%, #8C8796 60%);
-  -webkit-background-clip: text;
-  background-clip: text;
-  width: 1000px;
-  color: transparent;
-  animation: shineTitle 3s linear infinite;
-  @keyframes shineTitle {
-      0% {
-          background-position: 0px;
+const SCREEN_ROTATION_X = 100 * Math.PI / 180;
+
+function Model({ url } : { url: string }) {
+  const { scene, nodes } = useGLTF(url, true);
+  
+  useMemo(() => Object.values(nodes).forEach((obj) => {
+      if (obj.name === "Cube001_3" || obj.name === "Screen") {
+        obj.rotateX(SCREEN_ROTATION_X);
       }
-      100% {
-          background-position: 1000px;
-      }
-  }
-`
+      (obj as any).isMesh && Object.assign(obj, { castShadow: true, receiveShadow:true  })
+  }), [nodes])
+  return <primitive object={scene} dispose={null} />;
+}
+
+function ModelWrapper() {
+  const modelRef = useRef<any>();
+  const prevHeight = useRef<number>(0);
+
+  useEffect(() => {
+    if (modelRef.current) {
+    window.addEventListener("scroll", (e) => {
+        let rotationX = modelRef
+        .current
+        .children[0]
+        .children[0]
+        .children[0]
+        .children[0]
+        .children[1].rotation.x; 
+
+        console.log((window.scrollY - prevHeight.current) );
+        let addition = (window.scrollY - prevHeight.current) * -0.005; 
+        let scaleAddition = (window.scrollY - prevHeight.current) * 0.001; 
+        let positionAddition = (window.scrollY - prevHeight.current) * 0.0025;
+
+        if (rotationX + addition < 0 && rotationX + addition > -2.55) {
+          const laptop = modelRef
+          .current
+          .children[0]
+          .children[0]
+          .children[0]
+          .children[0]; 
+
+          laptop.children[1].rotateX(addition);
+          laptop.scale.x += scaleAddition;
+          laptop.scale.y += scaleAddition;
+          laptop.scale.z += scaleAddition;
+
+          laptop.position.z += positionAddition / 2; 
+          laptop.position.y -= positionAddition * 2;
+        } 
+        prevHeight.current = window.scrollY;
+      });
+    }
+  }, []);
+
+  return (
+    <animated.mesh
+    ref={modelRef}
+    rotation={[Math.PI / 6, 0, 0]}
+    scale={3}
+    position={[0, -2, -2]}
+  >
+     <OrbitControls
+            enableZoom={false}
+            minPolarAngle={Math.PI / 2.75}
+            maxPolarAngle={Math.PI / 2.75}
+            rotateSpeed={0.5}
+            enableRotate={false}
+            autoRotate={false}
+            enablePan={false}
+            autoRotateSpeed={1}
+        />
+    <Model 
+      url={"/laptop.glb"}
+    />
+    {/* <hemisphereLight intensity={1.5}
+      groundColor="gold"/> */}
+    <Lights/>
+  </animated.mesh>
+  )
+}
+
+const MouseAnimationContainer = styled.div`
+    animation: float 5s ease-in-out infinite;
+    @keyframes float {
+        0% {
+            transform: translateY(0px);
+        }
+        50% {
+            transform: translateY(4px);
+        }
+        100% {
+            transform: translateY(0px);
+        }
+    }
+`;
 
 export default function Home() {
   return (
-    <Animate>
-      <header className="mt-6 p-8 w-screen max-w-screen-xl flex flex-col items-center">
-        <div>
-        <ShimmeringH1 
-          style={{
-            lineHeight: "1.3",
-            letterSpacing: "-12px",
-          }}
-          className="text-9xl text-white">
-          Computer Science<br/>Academy
-        </ShimmeringH1>
-        <h2 
-          style={{
-            letterSpacing: "-2px"
-          }}
-          className="text-3xl mt-10 text-[rgba(255,255,255,0.75)]"
-        >
-          A South Brunswick High School <span className="text-white">Career Academy</span> experience directed by <span className="text-white">Steven Schiff</span>.
-        </h2>
-        </div>
-        <div className="mt-10 flex space-x-3 justify-center">
-        <Animate.Element
-            onDeactivatedClasses="-translate-x-[100px]"
-            onActivatedClasses="translate-x-0"
-            className="transition-all duration-700"
-          >
-            <Image 
-              className="rounded-[45px] border-solid border-[12px] border-[rgba(255,255,255,0.1)]"
-              src="/projects/weather-1.png"
-              width={275}
-              height={550}
-              alt="Weather 1"
-            />  
-          </Animate.Element>
-          <Animate.Element
-            onDeactivatedClasses="-translate-x-[100px]"
-            onActivatedClasses="translate-x-0"
-            className="transition-all duration-500"
-          >
-            <Image 
-              className="rounded-[45px] border-solid border-[12px] border-[rgba(255,255,255,0.1)]"
-              src="/projects/weather-0.png"
-              width={275}
-              height={550}
-              alt="Weather 0"
-            />  
-          </Animate.Element>
-          <div className="flex flex-col space-y-[20px]">
-            <Animate.Element
-              onDeactivatedClasses="translate-y-[100px]"
-              onActivatedClasses="translate-y-0"
-              className="transition-all duration-500"
-            >
-              <Image 
-                className="rounded-[45px] border-solid border-[12px] border-[rgba(255,255,255,0.1)]"
-                src="/projects/keyboard.png"
-                width={500}
-                height={250}
-                alt="Keyboard"
-              />  
-            </Animate.Element>
-            <Animate.Element
-              onDeactivatedClasses="translate-y-[100px]"
-              onActivatedClasses="translate-y-0"
-              className="transition-all duration-500"
-              resetAfterTriggered={false}
-            >
-              <Image 
-                className="rounded-[45px] border-solid border-[12px] border-[rgba(255,255,255,0.1)]"
-                src="/projects/keyboard.png"
-                width={500}
-                height={250}
-                alt="Keyboard"
-              />  
-            </Animate.Element>
-          </div>
-          <Animate.Element
-            onDeactivatedClasses="translate-x-[100px]"
-            onActivatedClasses="translate-x-0"
-            className="transition-all duration-700"
-          >
-            <Image 
-              className="rounded-[45px] border-solid border-[12px] border-[rgba(255,255,255,0.1)]"
-              src="/projects/notflix.png"
-              width={275}
-              height={550}
-              alt="Notflix"
-            />  
-          </Animate.Element>
-        </div>
-      </header>
-      <section>
+    <main style={{ height: 2000,  }}>
+      <div className="absolute left-1/2 -translate-x-1/2 top-[150px] flex space-x-3 justify-center items-center">
+        <MouseAnimationContainer>
+          <Image
+            src="/svg/scroll-down.svg"
+            width={30}
+            height={30}
+            alt="Scroll Down"
+          />
+        </MouseAnimationContainer>
+        <p>
+          Scroll Down to Begin...
+        </p>
+      </div>
+
+      {/* look into using demand */}
+      <Canvas
         
-      </section>
-    </Animate>
+        frameloop="always" 
+        gl={{ 
+            antialias: true,
+            autoClear: true,
+        }}
+        onCreated={(ctx) => { 
+            ctx.gl.toneMapping = THREE.ACESFilmicToneMapping; 
+            ctx.gl.toneMappingExposure = 1;
+        }}
+        style={{ 
+          height: "calc(100vh)",
+          width: "100vw", 
+          pointerEvents: "none",
+          position: "fixed",
+          left: 0,
+          top: 0
+        }}
+        dpr={2}
+        camera={{}}>
+         <ModelWrapper/>
+     
+        </Canvas>
+    </main>
   );
 
 }
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
                 // backdropFilter: "blur(10px)",
                 // WebkitBackdropFilter: "blur(10px)"
             }}
-            className="py-3 bg-black px-3 md:px-9 fixed top-0 left-0 right-0 flex justify-center items-center z-50">
+            className="py-3 px-3 md:px-9 fixed top-0 left-0 right-0 flex justify-center items-center z-50">
             {/* <Link href={"/"}>
                 <Image
                     src={"/logo.png"}
