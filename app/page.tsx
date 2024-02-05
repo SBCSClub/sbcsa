b99cbb7d1ff094ed324947fdc8c08fa0119441c3
mahitmehta <mahit.py@gmail.com>
2024-02-04 21:00:46 -0500

    Landing Page

--- app/page.tsx
+++ app/page.tsx
@@ -27,27 +27,65 @@ background-image: linear-gradient(90deg, #8C8796 0%, white 40%, #8C8796 60%);
 export default function Home() {
   return (
     <Animate>
-      <header className="mt-6 p-8 w-screen max-w-screen-xl flex flex-col items-center">
-        <div>
-        <ShimmeringH1 
-          style={{
-            lineHeight: "1.3",
-            letterSpacing: "-12px",
-          }}
-          className="text-9xl text-white">
-          Computer Science<br/>Academy
-        </ShimmeringH1>
-        <h2 
-          style={{
-            letterSpacing: "-2px"
-          }}
-          className="text-3xl mt-10 text-[rgba(255,255,255,0.75)]"
+      <header className="overflow-hidden relative p-8 w-screen h-screen justify-center max-w-screen-xl flex flex-col items-center">
+        <Animate.Element 
+          className='absolute w-full h-full top-0 left-0'
+          onActivatedClasses='scale-[2] duration-[1s] delay-500'
+          onDeactivatedClasses='scale-[0.75]'
         >
-          A South Brunswick High School <span className="text-white">Career Academy</span> experience directed by <span className="text-white">Steven Schiff</span>.
-        </h2>
-        </div>
-        <div className="mt-10 flex space-x-3 justify-center">
+          <Image 
+            className='pointer-events-none'
+            layout='fill'
+            objectFit="contain"
+            src="/macbook-air.png"
+            alt="Macbook"
+          />
+        </Animate.Element>
         <Animate.Element
+          className='z-50' 
+          onActivatedClasses='scale-1 duration-500 delay-500'
+          onDeactivatedClasses='scale-[0.5]'
+        >
+        <Animate.Element 
+          onDeactivatedClasses='opacity-0'
+          onActivatedClasses='opacity-100 transition-opacity duration-500'
+        >
+          <Image 
+              width={150}
+              height={150}
+              alt="CSA Logo"
+              src="/logo.png"
+          />
+        </Animate.Element>
+        <Animate.Element
+          onDeactivatedClasses='opacity-0'
+          onActivatedClasses='opacity-100 transition-opacity duration-500'
+        >
+          <ShimmeringH1 
+            style={{
+              lineHeight: "1.3",
+              letterSpacing: "-12px",
+            }}
+            className="text-9xl text-white">
+            Computer Science<br/>Academy
+          </ShimmeringH1>
+        </Animate.Element>
+        <Animate.Element
+          onDeactivatedClasses='opacity-0'
+          onActivatedClasses='opacity-100 transition-opacity duration-500 delay-300'
+        >
+          <h2 
+            style={{
+              letterSpacing: "-2px"
+            }}
+            className="text-3xl mt-10 text-[rgba(255,255,255,0.75)]"
+          >
+            A South Brunswick High School <span className="text-white">Career Academy</span> experience directed by <span className="text-white">Steven Schiff</span>.
+          </h2>
+        </Animate.Element>
+        </Animate.Element>
+        <div className="mt-10 flex space-x-3 justify-center">
+        {/* <Animate.Element
             onDeactivatedClasses="-translate-x-[100px]"
             onActivatedClasses="translate-x-0"
             className="transition-all duration-700"
@@ -114,7 +152,7 @@ export default function Home() {
               height={550}
               alt="Notflix"
             />  
-          </Animate.Element>
+          </Animate.Element> */}
         </div>
       </header>
       <section>
